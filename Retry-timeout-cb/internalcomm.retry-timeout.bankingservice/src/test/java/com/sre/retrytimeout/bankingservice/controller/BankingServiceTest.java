b07247377abcd0b0f314a303af2c6a69ad6ebf3e
singaraju-sreedhar <singaraju.sreedhar@101digital.io>
2023-04-11 12:18:12 +0530

    Best practise
    Added values for best practise suggestions  for Circuitbreaker

---
+++ Retry-timeout-cb/internalcomm.retry-timeout.bankingservice/src/test/java/com/sre/retrytimeout/bankingservice/controller/BankingServiceTest.java
@@ -0,0 +1,22 @@
+package com.sre.retrytimeout.bankingservice.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankingServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+}
